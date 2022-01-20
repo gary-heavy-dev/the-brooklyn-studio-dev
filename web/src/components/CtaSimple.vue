8cc260583927edb7998e7af1598d4d7a865d6380
Zack Lerner <zack@zacklerner.com>
2022-01-20 16:50:08 -0500

    Add aria label to ctaSimple module link

--- web/src/components/CtaSimple.vue
+++ web/src/components/CtaSimple.vue
@@ -22,6 +22,7 @@
             v-if="content.link"
             class="upper button mt-60"
             :to="content.link"
+            :aria-label="content.heading ? 'Link to ' + content.heading : content.linkText + ' Link'"
           >{{ content.linkText }}</g-link>
         </div>
       </div>
