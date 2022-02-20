daad701fd88ad86c7af9a02df7aa6b632502553c
Zack Lerner <zack@zacklerner.com>
2022-02-20 08:43:08 -0500

    Reenable dotlottie player

--- web/src/components/HomepageHero.vue
+++ web/src/components/HomepageHero.vue
@@ -6,7 +6,7 @@
       <div class="flex ai-c jc-c h-100 w-100 pos-abs z-1">
         <div class="container text-center p-100">
           <!-- <Logo class="white" /> -->
-          <div id="logoAnimation"></div>
+          <dotlottie-player src="https://assets6.lottiefiles.com/dotlotties/dlf10_hdnplzwy.lottie" autoplay />
         </div>
       </div>
       <BaseImage
@@ -22,7 +22,6 @@
 
 <script>
 import Logo from '~/components/Logo'
-// import { JLottieAnimation } from "./JLottie";
 
 export default {
   components: {
@@ -39,17 +38,10 @@ export default {
     }
   },
   mounted() {
-    window.jlottie = require('@lottiefiles/jlottie')
-
-    jlottie.loadAnimation({
-      // container: this.$refs.logoAnimation,
-      container: document.getElementById('logoAnimation'),
-      loop: false,
-      autoplay: true,
-      useWebWorker: false,
-      // path: 'https://assets6.lottiefiles.com/dotlotties/dlf10_hdnplzwy.lottie',
-      path: 'https://assets5.lottiefiles.com/datafiles/UY9mEPXUFRiLsvy/data.json'
-    })
+    // Import Dotlottie Player Component
+    if (typeof window !== 'undefined') {
+      const LottiePlayer = require('@dotlottie/player-component')
+    }
   }
 }
 </script>
