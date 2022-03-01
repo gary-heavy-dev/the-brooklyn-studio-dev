bd252bf7f62f8066b686b096fe2ddbbf54bb3cf6
Zack Lerner <zack@zacklerner.com>
2022-03-01 17:10:36 -0500

    Update proust questionnaire type styles

--- web/src/components/TeamMemberQuestionnaire.vue
+++ web/src/components/TeamMemberQuestionnaire.vue
@@ -12,113 +12,113 @@
         v-if="content.perfectHappiness"
         class="mb-40"
       >
-        <h4>What is your idea of perfect happiness?</h4>
-        <span class="color--navy">{{ content.perfectHappiness }}</span>
+        <h4 class="color--navy">What is your idea of perfect happiness?</h4>
+        <span class="color--gray-tertiary">{{ content.perfectHappiness }}</span>
       </div>
       <div
         v-if="content.greatestFear"
         class="mb-40"
       >
-        <h4>What is your greatest fear?</h4>
-        <span class="color--navy">{{ content.greatestFear }}</span>
+        <h4 class="color--navy">What is your greatest fear?</h4>
+        <span class="color--gray-tertiary">{{ content.greatestFear }}</span>
       </div>
       <div
         v-if="content.greatestExtravagance"
         class="mb-40"
       >
-        <h4>What is your greatest extravagance?</h4>
-        <span class="color--navy">{{ content.greatestExtravagance }}</span>
+        <h4 class="color--navy">What is your greatest extravagance?</h4>
+        <span class="color--gray-tertiary">{{ content.greatestExtravagance }}</span>
       </div>
       <div
         v-if="content.talent"
         class="mb-40"
       >
-        <h4>What talent would you most like to have?</h4>
-        <span class="color--navy">{{ content.talent }}</span>
+        <h4 class="color--navy">What talent would you most like to have?</h4>
+        <span class="color--gray-tertiary">{{ content.talent }}</span>
       </div>
       <div
         v-if="content.favoriteColor"
         class="mb-40"
       >
-        <h4>What is your favorite color?</h4>
-        <span class="color--navy">{{ content.favoriteColor }}</span>
+        <h4 class="color--navy">What is your favorite color?</h4>
+        <span class="color--gray-tertiary">{{ content.favoriteColor }}</span>
       </div>
       <div
         v-if="content.favoriteVirtue"
         class="mb-40"
       >
-        <h4>What is your favorite virtue?</h4>
-        <span class="color--navy">{{ content.favoriteVirtue }}</span>
+        <h4 class="color--navy">What is your favorite virtue?</h4>
+        <span class="color--gray-tertiary">{{ content.favoriteVirtue }}</span>
       </div>
       <div
         v-if="content.livingArchitect"
         class="mb-40"
       >
-        <h4>Which living architect or designer do you most admire?</h4>
-        <span class="color--navy">{{ content.livingArchitect }}</span>
+        <h4 class="color--navy">Which living architect or designer do you most admire?</h4>
+        <span class="color--gray-tertiary">{{ content.livingArchitect }}</span>
       </div>
       <div
         v-if="content.deadArchitect"
         class="mb-40"
       >
-        <h4>Which dead architect or designer do you most admire?</h4>
-        <span class="color--navy">{{ content.deadArchitect }}</span>
+        <h4 class="color--navy">Which dead architect or designer do you most admire?</h4>
+        <span class="color--gray-tertiary">{{ content.deadArchitect }}</span>
       </div>
       <div
         v-if="content.buildingTrait"
         class="mb-40"
       >
-        <h4>What is the trait that you most appreciate in a building?</h4>
-        <span class="color--navy">{{ content.buildingTrait }}</span>
+        <h4 class="color--navy">What is the trait that you most appreciate in a building?</h4>
+        <span class="color--gray-tertiary">{{ content.buildingTrait }}</span>
       </div>
       <div
         v-if="content.projectType"
         class="mb-40"
       >
-        <h4>What is your favorite type of project to work on?</h4>
-        <span class="color--navy">{{ content.projectType }}</span>
+        <h4 class="color--navy">What is your favorite type of project to work on?</h4>
+        <span class="color--gray-tertiary">{{ content.projectType }}</span>
       </div>
       <div
         v-if="content.favoriteAuthor"
         class="mb-40"
       >
-        <h4>Who is your favorite author?</h4>
-        <span class="color--navy">{{ content.favoriteAuthor }}</span>
+        <h4 class="color--navy">Who is your favorite author?</h4>
+        <span class="color--gray-tertiary">{{ content.favoriteAuthor }}</span>
       </div>
       <div
         v-if="content.favoriteArtist"
         class="mb-40"
       >
-        <h4>Who is your favorite artist?</h4>
-        <span class="color--navy">{{ content.favoriteArtist }}</span>
+        <h4 class="color--navy">Who is your favorite artist?</h4>
+        <span class="color--gray-tertiary">{{ content.favoriteArtist }}</span>
       </div>
       <div
         v-if="content.historicalFigure"
         class="mb-40"
       >
-        <h4>Which historical figure do you most identify with?</h4>
-        <span class="color--navy">{{ content.historicalFigure }}</span>
+        <h4 class="color--navy">Which historical figure do you most identify with?</h4>
+        <span class="color--gray-tertiary">{{ content.historicalFigure }}</span>
       </div>
       <div
         v-if="content.whereToLive"
         class="mb-40"
       >
-        <h4>Where would you like to live?</h4>
-        <span class="color--navy">{{ content.whereToLive }}</span>
+        <h4 class="color--navy">Where would you like to live?</h4>
+        <span class="color--gray-tertiary">{{ content.whereToLive }}</span>
       </div>
       <div
         v-if="content.qualityInOthers"
         class="mb-40"
       >
-        <h4>What quality do you most admire in others?</h4>
-        <span class="color--navy">{{ content.qualityInOthers }}</span>
+        <h4 class="color--navy">What quality do you most admire in others?</h4>
+        <span class="color--gray-tertiary">{{ content.qualityInOthers }}</span>
       </div>
       <div
         v-if="content.twoBooks"
         class="mb-40"
       >
-        <h4>Stranded on a desert island, you have two books in your possession. What are they?</h4>
-        <span class="color--navy">{{ content.twoBooks }}</span>
+        <h4 class="color--navy">Stranded on a desert island, you have two books in your possession. What are they?</h4>
+        <span class="color--gray-tertiary">{{ content.twoBooks }}</span>
       </div>
     </div>
   </div>
@@ -152,7 +152,10 @@ export default {
   }
 
   &__questions {
-    font-weight: 500;
+
+    h4 {
+      font-weight: 500;
+    }
   }
 }
 </style>
