e33ae2a8b9569e81fd51a528a0e6e7b8573aefc8
Zack Lerner <zack@zacklerner.com>
2022-03-04 17:41:59 -0500

    Refactor intro animation

--- web/src/components/HomepageHero.vue
+++ web/src/components/HomepageHero.vue
@@ -5,24 +5,14 @@
       @dblclick="closeIntro"
     >
       <div class="flex ai-c jc-c h-100 w-100 pos-abs z-1">
-        <!-- https://assets3.lottiefiles.com/packages/lf20_3wz8udr0.json -->
-        <!-- https://assets10.lottiefiles.com/packages/lf20_ey5jrrlx.json -->
-        <!-- <vLottiePlayer
-          name="workoutMonkeyAnim"
-          loop
-          :animationData="require('./assets/workout-monkey.json')"
-        /> -->
-        <!-- <v-lottie-player
-          name="scooterAnim"
-          @animControl="play"
-          path="https://assets10.lottiefiles.com/packages/lf20_ey5jrrlx.json"
-        /> -->
         <lottie-animation
+          class="w-100"
           ref="anim"
           :animationData="animationData"
           :autoPlay="false"
         />
       </div>
+      <div id="introImageOverlay"></div>
       <img
         v-if="featuredImage"
         :src="featuredImage.asset.url"
@@ -34,7 +24,8 @@
 
 <script>
 import Logo from '~/components/Logo'
-import LogoAnimation from '~/components/lottie/desktop-logo-animation.json'
+import DesktopLogoAnimation from '~/components/lottie/desktop-logo-animation.json'
+import MobileLogoAnimation from '~/components/lottie/mobile-logo-animation.json'
 import LottieAnimation from 'lottie-web-vue'
 
 export default {
@@ -44,10 +35,18 @@ export default {
   },
   data() {
     return {
-      animationData: LogoAnimation
+      desktopAnimation: DesktopLogoAnimation,
+      mobileAnimation: MobileLogoAnimation
     }
   },
   computed: {
+    animationData() {
+      if (window.innerWidth <= 1024) {
+        return this.mobileAnimation
+      } else {
+        return this.desktopAnimation
+      }
+    },
     featuredImage() {
       const ims = this.$static.page.heroImages.images.length
       const i = Math.floor(Math.random() * ims)
@@ -66,7 +65,7 @@ export default {
       }, 500);
       setTimeout(() => {
         document.getElementById('introAnimation').classList.add('initial-fade')
-      }, 2900);
+      }, 2800);
     }
   },
 }
@@ -94,12 +93,22 @@ body.close-intro .homepage-hero {
     height: 100%;
   }
 
-  &.initial-fade {
-    background-color: black;
+  #introImageOverlay {
+    opacity: 0;
+    background: rgb(0,0,0);
+    background: linear-gradient(0deg, rgba(0,0,0,0.15) 0%, rgba(0,0,0,0.15) 50%, rgba(0,0,0,0.70) 100%);
+    transition: opacity 1600ms ease-in-out;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
 
-    img.loaded {
-      opacity: 0.7;
-      transition-duration: 1.35s;
+  &.initial-fade {
+
+    #introImageOverlay {
+      opacity: 1;
     }
   }
 
@@ -107,6 +116,9 @@ body.close-intro .homepage-hero {
   img {
     opacity: 0;
     transition: opacity 750ms linear;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
 
     &.loaded {
       opacity: 1;
