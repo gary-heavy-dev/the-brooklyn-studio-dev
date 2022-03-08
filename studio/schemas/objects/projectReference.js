2c65f607daadaf495ccf7490cbc5ee25de0a56c3
Zack Lerner <zack@zacklerner.com>
2022-03-08 16:37:53 -0500

    Update thumbnail description text

--- studio/schemas/objects/projectReference.js
+++ studio/schemas/objects/projectReference.js
@@ -14,7 +14,7 @@ export default {
       name: 'altThumbnail',
       title: 'Thumbnail',
       type: 'mainImage',
-      description: 'Please upload the thumbnail image for the project here. The recommended size is 1226px by 818px',
+      description: 'Please upload the thumbnail image for the project here. The recommended size is 1226px by 981px',
       validation: Rule => Rule.required()
     }
   ],
