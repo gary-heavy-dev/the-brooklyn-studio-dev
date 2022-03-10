26365777d25e98ab2ac93a458f38808cb98d8688
Zack Lerner <zack@zacklerner.com>
2022-03-10 13:10:51 -0500

    Add page titles and default descriptions

--- web/src/pages/Careers.vue
+++ web/src/pages/Careers.vue
@@ -8,6 +8,49 @@ import Careers from '~/components/Careers'
 export default {
   components: {
     Careers
+  },
+  metaInfo() {
+    return {
+      title: 'Careers',
+      titleTemplate: '%s | The Brooklyn Studio',
+      meta: [
+        {
+          key: 'description',
+          name: 'description',
+          content: 'The Brooklyn Studio is an interdisciplinary architecture and design firm committed to restoring and enhancing New York City\'s historic fabric.'
+        },
+        // {
+        //   key: 'og:title',
+        //   name: 'og:title',
+        //   content: (this.casestudy ? this.casestudy.title : '') + ' ' + this.$static.metadata.siteName
+        // },
+        // {
+        //   key: 'og:description',
+        //   name: 'og:description',
+        //   content: this.casestudy ? this.casestudy.description : '',
+        // },
+        // {
+        //   key: 'og:image',
+        //   name: 'og:image',
+        //   content: this.casestudy && this.casestudy.mainImage.asset ? this.casestudy.mainImage.asset.url : (this.$static.settings.ogImage ? this.$static.settings.ogImage.asset.url : '')
+        // },
+        // {
+        //   key: 'twitter:title',
+        //   name: 'twitter:title',
+        //   content: (this.casestudy ? this.casestudy.title : '') + ' ' + this.$static.metadata.siteName
+        // },
+        // {
+        //   key: 'twitter:description',
+        //   name: 'twitter:description',
+        //   content: this.casestudy ? this.casestudy.description : '',
+        // },
+        // {
+        //   key: 'twitter:image',
+        //   name: 'twitter:image',
+        //   content: this.casestudy && this.casestudy.mainImage.asset ? this.casestudy.mainImage.asset.url : (this.$static.settings.ogImage ? this.$static.settings.ogImage.asset.url : '')
+        // }
+      ]
+    }
   }
 }
 </script>
