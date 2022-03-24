aefa03e759b8cbff484c9d756efc9db3b7402c6b
Zack Lerner <zack@zacklerner.com>
2022-03-24 11:22:36 -0400

    Fix build error related to decodeEntities function

--- web/src/utils/decodeEntities.js
+++ web/src/utils/decodeEntities.js
@@ -1,20 +1,22 @@
 function decodeEntities(str) {
-  const element = document.createElement('div')
+  if (typeof document !== 'undefined') {
+    const element = document.createElement('div')
 
-  if(str && typeof str === 'string') {
-    // strip script/html tags
-    str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '')
-    str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '')
-    element.innerHTML = str
-    str = element.textContent
-    element.textContent = ''
-  //   console.log('poopsie')
-  // } else {
-  //   console.log('oopsie')
-  //   str = 'oopsie'
+    if(str && typeof str === 'string') {
+      // strip script/html tags
+      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '')
+      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '')
+      element.innerHTML = str
+      str = element.textContent
+      element.textContent = ''
+    //   console.log('poopsie')
+    // } else {
+    //   console.log('oopsie')
+    //   str = 'oopsie'
+    }
+
+    return str
   }
-
-  return str
 }
 
 export default decodeEntities 
