1263837f88c9572130a8ea0b77d41bea43bbf5bf
Zack Lerner <zack@zacklerner.com>
2022-07-18 18:25:04 -0400

    BKS-14 Reformat Project Details Module

--- web/src/components/ProjectDetails.vue
+++ web/src/components/ProjectDetails.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="project-details flex fd-c background--gray-secondary p-100">
     <div class="project-details__inner grid grid--12-desktop container">
-      <div class="project-details__copy col-span--6">
+      <div class="project-details__copy col-span--7">
         <div class="project-details__intro pt-20 mb-60">
           <p v-if="content.detailsIntro">{{ content.detailsIntro }}</p>
         </div>
-        <div class="project-details__info grid grid--6-desktop color--gray-tertiary">
+        <div class="project-details__info grid grid--7-desktop color--gray-tertiary">
           <div class="col-span--3">
             <h3 class="mb-20">Building Information</h3>
             <div
@@ -16,7 +16,7 @@
               <div class="xsmall mb-20">{{ stat.text }}</div>
             </div>
           </div>
-          <div class="col-span--3">
+          <div class="col-span--4">
             <h3 class="mb-20">Credits</h3>
             <div
               v-for="(credit, index) in content.detailsCredits"
@@ -28,7 +28,7 @@
           </div>
         </div>
       </div>
-      <div class="project-details__gallery-wrapper col-span--6">
+      <div class="project-details__gallery-wrapper col-span--5">
         <div
           class="project-details__gallery-inner text-center pos-rel"
           v-view="startSwiper"
@@ -48,8 +48,8 @@
                 :src="image"
                 :lazy="true"
                 :sizes="sizes"
-                :x="390"
-                :y="496"
+                :x="2"
+                :y="3"
                 :caption="image.caption"
                 :captionStyle="image.captionStyle"
               />
@@ -103,12 +103,12 @@ export default {
         }
       },
       sizes: {
-        mobile: 480,
-        tablet: 768,
-        laptop: 1024,
-        desktop: 591,
-        hd: 792,
-        fourK: 1584
+        mobile: 413,
+        tablet: 660,
+        laptop: 855,
+        desktop: 489,
+        hd: 656,
+        fourK: 1313
       }
     }
   },
