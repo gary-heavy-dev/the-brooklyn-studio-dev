c0d63f2a1286cc29a20cfa93d704ba42e1285b48
Zack Lerner <zack@zacklerner.com>
2024-04-08 14:40:02 -0400

    Update cli config

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -3,17 +3,17 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
-    dataset: process.env.SANITY_STUDIO_API_DATASET
-  }
-  // graphql: [
-  //   {
-  //     id: "production-workspace",
-  //     workspace: "production-workspace",
-  //   },
-  //   {
-  //     id: "staging-workspace",
-  //     workspace: "staging-workspace",
-  //   },
-  // ]
+    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID
+    // dataset: process.env.SANITY_STUDIO_API_DATASET
+  },
+  graphql: [
+    {
+      id: 'production',
+      workspace: 'production'
+    },
+    {
+      id: 'staging',
+      workspace: 'staging'
+    }
+  ]
 })
