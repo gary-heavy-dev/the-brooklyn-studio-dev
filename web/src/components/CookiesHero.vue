9dd44f2018789bd0b334ecb22d2983fd5583c06f
Walker Adkins <walkeradkins@gmail.com>
2024-12-05 10:55:25 -0600

    fix image wrapper.

--- web/src/components/CookiesHero.vue
+++ web/src/components/CookiesHero.vue
@@ -1,15 +1,17 @@
 <template>
   <div :class="['cookies--hero', 'pos-rel', { 'mb-100': margin }, { overlay: !cookies }]">
-    <BaseImage
-      v-if="image"
-      class="img-cover"
-      :lazy="true"
-      :src="image"
-      :x="1440"
-      :y="500"
-      :caption="image.caption"
-      :captionStyle="image.captionStyle"
-    />
+    <div class="image__wrapper">
+      <BaseImage
+        v-if="image"
+        class="img-cover"
+        :lazy="true"
+        :src="image"
+        :x="1440"
+        :y="500"
+        :caption="image.caption"
+        :captionStyle="image.captionStyle"
+      />
+    </div>
     <div class="cookies-hero__title pt-80 z-1 w-100 flex jc-c color--white background--navy">
       <h1 v-if="title" :class="['h1', 'color--white', 'container', { cookies: cookies }]">
         <div>{{ title }}</div>
@@ -57,8 +59,8 @@ export default {
   @include laptop-down {
     min-height: 176px;
 
-    .img-cover {
-      height: 45vw;
+    .image__wrapper {
+      height: 34.72vh;
       min-height: 300px;
     }
 
